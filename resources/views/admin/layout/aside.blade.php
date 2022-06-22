119f24d4c654105668b827bdc6c624ff69af98db
hamde abo khadra <hamdeahmed01221@gmail.com>
2022-06-22 18:09:44 +0200

    Backend Slider Option Setup

--- resources/views/admin/layout/aside.blade.php
+++ resources/views/admin/layout/aside.blade.php
@@ -74,25 +74,19 @@
                 </ul>
             </li>
 
-            <li class="header nav-small-cap">User Interface</li>
-
-            <li class="treeview">
+            <li class="treeview {{ ($prefix == '/slider')?'active':'' }}  ">
                 <a href="#">
-                    <i data-feather="grid"></i>
-                    <span>Components</span>
+                    <i data-feather="file"></i>
+                    <span>Slider</span>
                     <span class="pull-right-container">
               <i class="fa fa-angle-right pull-right"></i>
             </span>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="components_alerts.html"><i class="ti-more"></i>Alerts</a></li>
-                    <li><a href="components_badges.html"><i class="ti-more"></i>Badge</a></li>
-                    <li><a href="components_buttons.html"><i class="ti-more"></i>Buttons</a></li>
-                    <li><a href="components_sliders.html"><i class="ti-more"></i>Sliders</a></li>
-                    <li><a href="components_dropdown.html"><i class="ti-more"></i>Dropdown</a></li>
-                    <li><a href="components_modals.html"><i class="ti-more"></i>Modal</a></li>
-                    <li><a href="components_nestable.html"><i class="ti-more"></i>Nestable</a></li>
-                    <li><a href="components_progress_bars.html"><i class="ti-more"></i>Progress Bars</a></li>
+                    <li class="{{ ($route == 'manage-slider')? 'active':'' }}"><a href="{{ route('manage-slider') }}"><i class="ti-more"></i>Manage Slider</a></li>
+
+
+
                 </ul>
             </li>
 
