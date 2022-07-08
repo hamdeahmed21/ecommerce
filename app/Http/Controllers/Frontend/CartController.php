2d2f6b83bfa8a4ae94abf36b9139527bfc0b3670
hamde abo khadra <hamdeahmed01221@gmail.com>
2022-07-08 19:57:29 +0200

    Frontend Coupon Apply With Ajax Setup

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Coupon;
 use App\Models\Product;
 use App\Models\Wishlist;
 use Gloudemans\Shoppingcart\Facades\Cart;
@@ -108,5 +109,52 @@ class CartController extends Controller
         }
 
     } // end method
+    public function CouponApply(Request $request){
 
+        $coupon = Coupon::where('coupon_name',$request->coupon_name)->where('coupon_validity','>=',Carbon::now()->format('Y-m-d'))->first();
+        if ($coupon) {
+
+            Session::put('coupon',[
+                'coupon_name' => $coupon->coupon_name,
+                'coupon_discount' => $coupon->coupon_discount,
+                'discount_amount' => round(Cart::total() * $coupon->coupon_discount/100),
+                'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100)
+            ]);
+
+            return response()->json(array(
+                'validity' => true,
+                'success' => 'Coupon Applied Successfully'
+            ));
+
+        }else{
+            return response()->json(['error' => 'Invalid Coupon']);
+        }
+
+    } // end method
+
+
+    public function CouponCalculation(){
+
+        if (Session::has('coupon')) {
+            return response()->json(array(
+                'subtotal' => Cart::total(),
+                'coupon_name' => session()->get('coupon')['coupon_name'],
+                'coupon_discount' => session()->get('coupon')['coupon_discount'],
+                'discount_amount' => session()->get('coupon')['discount_amount'],
+                'total_amount' => session()->get('coupon')['total_amount'],
+            ));
+        }else{
+            return response()->json(array(
+                'total' => Cart::total(),
+            ));
+
+        }
+    } // end method
+
+
+    // Remove Coupon
+    public function CouponRemove(){
+        Session::forget('coupon');
+        return response()->json(['success' => 'Coupon Remove Successfully']);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -280,4 +280,11 @@ Route::prefix('shipping')->group(function() {
 
 
 });
+// Frontend Coupon Option
+
+Route::post('/coupon-apply', [CartController::class, 'CouponApply']);
+
+Route::get('/coupon-calculation', [CartController::class, 'CouponCalculation']);
+
+Route::get('/coupon-remove', [CartController::class, 'CouponRemove']);
 
