7015715202628ee44f7d579a6847e289198066c6
hamde abo khadra <hamdeahmed01221@gmail.com>
2022-07-12 02:16:06 +0200

    route checkout

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -120,7 +120,7 @@
                                     <div class="pull-right"> <span class="text">Sub Total :</span>
                                         <span class='price'  id="cartSubTotal">  </span> </div>
                                     <div class="clearfix"></div>
-                                    <a href="checkout.html" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a> </div>
+                                    <a href="{{ route('checkout') }}" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a> </div>
                                 <!-- /.cart-total-->
 
                             </li>
